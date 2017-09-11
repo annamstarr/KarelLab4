4419eaceee8a45db68685fd522150ccdf6917278
annamstarr <31353295+annamstarr@users.noreply.github.com>
2017-09-11 11:56:45 -0700

    Karel Lab 4 day 1

--- BeeperBot.java
+++ BeeperBot.java
@@ -3,16 +3,34 @@ import kareltherobot.*;
 
 
 /**
- * @author :  D. Appel
+ * @author :  A. Starr
  */
 public class BeeperBot extends Robot
 {
     public BeeperBot(int st, int av, Direction dir, int numBeepers) {
         super(st, av, dir, numBeepers);
     }
-    
     public void runRace() {
-        
+        while (frontIsClear()) {
+            collectBepperSteeple();
+        }
     }
-   
+    public void collectBeeperSteeple() {
+        while (
+    }
+    public void 
+    public void findBeepers() {
+    faceEast();
+    while (!nextToABeeper()) {
+        move();
+    }
 }
+   public void faceEast() {
+    while (!facingEast()) {
+        turnLeft();
+    }
+}
+}
+
+
+
